746faac97083711d6e9bd3c52ae9922800d51d23
CaptainOfPhB <captainofphb@gmail.com>
2022-06-21 00:08:17 +0800

    use runAllTimers instead

--- packages/client/src/utils/debounce.test.ts
+++ packages/client/src/utils/debounce.test.ts
@@ -1,13 +1,15 @@
 import debounce from './debounce';
 
 jest.useFakeTimers();
+jest.spyOn(window, 'setTimeout');
 
 it('callback should be executed after delay time', () => {
   const fn = jest.fn();
   debounce(fn)();
   expect(fn).not.toBeCalled();
-  jest.advanceTimersByTime(300);
+  jest.runAllTimers();
   expect(fn).toHaveBeenCalledTimes(1);
+  expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 300);
 });
 
 it('callback should only be executed when the interval time is greater than the delay time', () => {
@@ -18,7 +20,7 @@ it('callback should only be executed when the interval time is greater than the 
   callback(123);
   callback(1234);
   callback(12345);
-  jest.advanceTimersByTime(300);
+  jest.runAllTimers();
   expect(callback).toHaveBeenCalledTimes(5);
   expect(fn).toHaveBeenCalledTimes(1);
   expect(fn).toBeCalledWith(12345);
